73fd3cbffab13bc071fb595be69bc9033f677105
Ravi kumar <raviroy44405@gmail.com>
2025-03-02 23:11:49 +0530

    Add files via upload

---
+++ Operator/arthmaticoperator.java
@@ -0,0 +1,11 @@
+public class arthmaticoperator {
+    public static void main(String[] args) {
+        System.out.println("here we perform Arthmatic operatation");
+        int a= 20; int b= 10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}

4911bf985e591cbb0e443baf8314ca7482478ad3
Ravi kumar <raviroy44405@gmail.com>
2025-03-02 23:16:09 +0530

    Add files via upload

---
+++ Operator/assignmentoperator.java
@@ -0,0 +1,9 @@
+public class assignmentoperator {
+    public static void main(String[] args) {
+        int p=10; int q= 8;
+        System.out.println(p+=q);
+        System.out.println(p-=q);
+        System.out.println(p*=q);
+        System.out.println(p/=q);
+    }
+}

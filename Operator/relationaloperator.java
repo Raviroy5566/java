057eaee0ec16a99bde5288ae349f6482b8ed4e0f
Ravi kumar <raviroy44405@gmail.com>
2025-03-02 23:14:47 +0530

    Add files via upload

---
+++ Operator/relationaloperator.java
@@ -0,0 +1,10 @@
+public class relationaloperator {
+    public static void main(String[] args) {
+        int a =32; int b= 8;
+        System.out.println(a==b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+    }
+}

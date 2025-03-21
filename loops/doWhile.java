27af8dc7fd761a75264ef927d03c528eac6f62d6
Ravi kumar <raviroy44405@gmail.com>
2025-03-21 22:57:11 +0530

    Add files via upload

---
+++ loops/doWhile.java
@@ -0,0 +1,12 @@
+
+
+public class doWhile {
+    public static void main(String[] args) {
+        int num = 1;
+        do{
+            System.out.println(num);
+            num++;
+        }while (num<=10);
+
+    }
+}

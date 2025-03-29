a3fbb494977dd46170d03dfbeba223270137de3c
Ravi kumar <raviroy44405@gmail.com>
2025-03-29 21:21:40 +0530

    Add files via upload

---
+++ Array/arrayExample.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class ArrayExam{
+void demoArray(){
+        int arr[] ={4,5,2,6,1};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+public class arrayExample{
+    public static void main(String args[]){
+        ArrayExam obj = new ArrayExam();
+        obj.demoArray();
+        
+    }
+}

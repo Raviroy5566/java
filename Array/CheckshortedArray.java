83f8ab9412df50dd3898ea2929b00b9075e488b8
Ravi kumar <raviroy44405@gmail.com>
2025-03-31 23:29:46 +0530

    Add files via upload

---
+++ Array/CheckshortedArray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CheckshortedArray{
+    static boolean isShorted(int arr[]){
+        boolean check = true;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<arr[i-1]){
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+
+
+
+ public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter size of array");
+    int n = sc.nextInt();
+    
+    int arr[]=new int[n];
+    System.out.println("Enter"+n+"element");
+    for(int i=0;i<arr.length;i++){
+        arr[i]= sc.nextInt();
+    }
+    System.out.println("is shorted "+isShorted(arr));
+
+}
+}
